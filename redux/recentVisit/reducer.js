ba29fec329fd05a20bf76215740a7a5563e3af48
sarthaksood9 <sarthaksood9@gmail.com>
2024-09-26 01:08:36 +0530

    recentvisit fix

--- redux/recentVisit/reducer.js
+++ redux/recentVisit/reducer.js
@@ -9,10 +9,17 @@ const initialState = {
 export const recentVisitReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_TO_RECENT:
+            const existingItem = state.items.find(item => item.id === action.payload.id);
+
+            if (existingItem) {
+                return state;
+            }
+
             return {
                 ...state,
                 items: [...state.items, action.payload],
             };
+
         default:
             return state;
     }
